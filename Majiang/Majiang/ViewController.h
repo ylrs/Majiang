b409dd39aaa98afb2a873652c71a3714abf4c6d1
YLRS <278051822@qq.com>
2017-08-24 15:29:38 +0800

    麻将项目创建

---
+++ Majiang/Majiang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Majiang
+//
+//  Created by YLRS on 2017/8/24.
+//  Copyright © 2017年 YLRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Majiang/Pods/Target Support Files/Pods-Majiang/Pods-Majiang-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Majiang : NSObject
+@end
+@implementation PodsDummy_Pods_Majiang
+@end
